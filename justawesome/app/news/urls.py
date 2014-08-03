987ae5db59b2705cabe3a28682692718249a93b5
Valdemar Rolfsen <valdemarrolfsen@Valdemars-MacBook-Pro.local>
2014-08-03 20:02:08 +0200

    Gjort noen små endringer, samt lagt til en article-side som kan åpnes hvis man trykker på en artikkel som man vil lese på hovedsiden

--- justawesome/app/news/urls.py
+++ justawesome/app/news/urls.py
@@ -5,6 +5,7 @@ viewpatterns = patterns('justawesome.app.news.views',
 	url(r'^nominate/$', 'nominate_view', name='nominate'),
 	url(r'^vote/$', 'vote_view', name='vote'),
 	url(r'^home/$', 'home_view', name='home'),
+	url(r'article/$', 'article_view', name='article'),
 )
 
 
--- justawesome/app/news/views.py
+++ justawesome/app/news/views.py
@@ -5,9 +5,12 @@ def nominate_view(request):
 	return render(request, 'nominate.jade')
 
 def vote_view(request):
-	return render(request, 'vote.jade', {'step_nr': 2, 'articles': Article.objects.all()[:3]})
+	return render(request, 'vote.jade', {'step_nr': 1, 'articles': Article.objects.all()[:3]})
 
 def home_view(request):
-	return render(request, 'home.jade')
+	return render(request, 'home.jade', {'articles': Article.objects.all()[:3], 'can_see': True})
+
+def article_view(request):
+	return render(request, 'article.jade', {'articles': Article.objects.all()})
 
 
